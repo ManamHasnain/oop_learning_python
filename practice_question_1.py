096592bdf34237d8ca51f386383b7c98c7903b67
ManamHasnain <alitheprogrammer11243@gmail.com>
2026-01-02 11:11:04 +0500

    one practice question solve by myself

---
+++ practice_question_1.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self,name,marks):
+        self.name=name 
+        self.marks=marks
+
+    def average(self):
+        total=0
+        for val in self.marks:
+            total+=val
+        average=total/len(self.marks)
+        print("hi ",self.name,"your total marks are ",total," and your average marks are ",average)
+
+stud1=Student("ali",[90,85,80])
+stud2=Student("hassan",[89,78,90,73])
+stud1.average()
+stud2.average()
