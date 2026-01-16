eac126624c9dec0e58bb2a18c81092068c9d4b35
ManamHasnain <alitheprogrammer11243@gmail.com>
2026-01-16 15:26:44 +0500

    super concept practiced and added

--- multilevel_inheritance.py
+++ multilevel_inheritance.py
@@ -1,4 +1,6 @@
 class Animal:
+    def __init__(self,color):
+        self.color=color
     # @ staticmethod
     def breath(self):
         print("this animal breaths")
@@ -7,16 +9,19 @@ class Mammal(Animal):
     def walk(self):
         print("mammal walks")
 class Dog(Mammal):
-    def __init__(self,name):
+    def __init__(self,name,color):
+        super().__init__(color)
         self.name=name
+        super().walk()
         
     # @ staticmethod
     def bark(self):
-        super().walk()
+        # super().walk()
         print("this dog bark woo woo!")
     
-my_dog=Dog("bruno")
+my_dog=Dog("bruno","black")
 print(my_dog.name)
+print(my_dog.color)
 my_dog.breath()
 # my_dog.walk()
 my_dog.bark()
--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -1,19 +1,27 @@
 class Father:
     surname="hussain"
+    def __init__(self,salary):
+        self.salary=salary
     def hair(self):
         print("I have black hair")
 class Mother:
     def eyes(self):
         print("I have brown eyes")
 class Child(Father,Mother):
-    def __init__(self, name):
+    def __init__(self, name, salary):
         self.name=name
-        super().__init__()
+        super().__init__(salary)
+        super().hair()
+
+
     def height(self):
         print("I am short heighted")
 
-child1=Child("ali")
+father=Father(19000)
+print(father.salary)
+child1=Child("ali",10000)
 print(child1.name)
+print(child1.salary)
 print(child1.surname)
 child1.hair()
 child1.eyes()
